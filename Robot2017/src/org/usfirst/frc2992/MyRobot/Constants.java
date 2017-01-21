b0f7e90ab8b19f9dd533baa96720e1b7b28aa719
Jackson Angerdina <jackson@angerdina.com>
2017-01-21 14:32:15 -0600

    robot width and length added to constants. coordinate math started. to be finished -Jackson

--- Robot2017/src/org/usfirst/frc2992/MyRobot/Constants.java
+++ Robot2017/src/org/usfirst/frc2992/MyRobot/Constants.java
@@ -2,4 +2,10 @@ package org.usfirst.frc2992.MyRobot;
 
 public class Constants {
 
+	public Constants(){
+		
+	}
+	
+	public final double robotWidth = 24.5; // length between middle of left and right drive bases
+	public final double robotLength = 23.5; // length between front and back wheels
 }
