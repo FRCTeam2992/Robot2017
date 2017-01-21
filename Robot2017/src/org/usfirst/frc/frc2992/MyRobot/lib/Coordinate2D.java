3c252f91755bf69ddffcd1d003b37a0a0a088a65
Jackson Angerdina <jackson@angerdina.com>
2017-01-21 14:10:00 -0600

    Updated Coordinate system with basic, but unfinished functions. -Jackson

--- Robot2017/src/org/usfirst/frc/frc2992/MyRobot/lib/Coordinate2D.java
+++ Robot2017/src/org/usfirst/frc/frc2992/MyRobot/lib/Coordinate2D.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.frc2992.MyRobot.lib;
 
 public class Coordinate2D {
-
+	
+	double x;
+	double y;
+	
+	public Coordinate2D(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setX_Coor(double x){
+		this.x = x;
+	}
+	
+	public void setY_Coor(double y){
+		this.y = y;
+	}
 }
